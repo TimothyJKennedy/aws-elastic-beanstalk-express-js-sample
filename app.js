cc661ded7fbeb50a3e338911c35c714a5dfc0b7d
TJ Kennedy <tkennedy1285@gmail.com>
2024-03-11 23:33:32 -0400

    Final Update
    Changing output to completion statement with date of completion

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('another test'));
+app.get('/', (req, res) => res.send('I have officially completed the "Create a continuous Pipeline Tutorial" on 3-11-24'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
